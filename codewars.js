6d48d7c30620efd4b2164a34de5a303772a0408e
j p <jp@jp.jp>
2022-10-06 22:27:09 +0200

    battleship validator

--- codewars.js
+++ codewars.js
@@ -1,6 +1,69 @@
-// Just a place for me to write codewars solutions and log them
+// Codewars solutions
 
-// Greed is Good
+// Battleship field validator - 3kyu
+function validateBattlefield(field) {
+  const ShipSizeAmountDict = {
+    4: 1,
+    3: 2,
+    2: 3,
+    1: 4,
+  }
+  
+  const evaluateShip = (y, x, shipSize) => {
+    shipSize++;
+    const top = { y: y === 0 ? 0 : y - 1, x };
+    const right = { x: x === 9 ? 9 : x + 1, y };
+    const bottom = { y: y === 9 ? 9 : y + 1, x };
+    const left = { x: x === 0 ? 0 : x - 1, y };
+
+    [top, left, bottom, right].forEach((n) => {
+      if (field[n.y][n.x]) {
+        field[n.y][n.x] = 0;
+        shipSize = evaluateShip(n.y, n.x, shipSize);
+      }
+    })
+    return shipSize;
+  }
+  
+  field.forEach((row, y) => { 
+    row.forEach((col, x) => {
+      if (field[y][x]) {
+        field[y][x] = 0;
+        let shipSize = 0;
+        shipSize = evaluateShip(y, x, shipSize);
+        if (ShipSizeAmountDict[shipSize]) {
+          ShipSizeAmountDict[shipSize]--;
+        }
+      }
+    })
+  });
+
+  return Object.values(ShipSizeAmountDict).every((shipAmount) => shipAmount === 0);
+}
+
+// Nesting Structure Comparison - 4kyu
+Array.prototype.sameStructureAs = function (other) {
+  
+  const serializeStructure = (array, positionX, positionY, string) => {
+    string += `x:${positionX}y:${positionY}, `;
+
+    array.forEach((element, nextPositionX) => {
+      string += Array.isArray(element) 
+      ? serializeStructure(element, nextPositionX, positionY + 1, string)
+      : 'false, ';
+    });
+
+    return string;
+  }
+
+  const thisArrayStructure = Array.isArray(this) ? serializeStructure(this, 0, 0, '') : false;
+  const otherArrayStructure = Array.isArray(other) ? serializeStructure(other, 0, 0, '') : false;
+
+  return thisArrayStructure === otherArrayStructure;
+};
+
+
+// Greed is Good - kyu
 function score(values) {
   const calculateOnesAndFives = (value, factor) =>
     factor * (value === 1 ? 100 : value === 5 ? 50 : 0);
