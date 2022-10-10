3af305ff8fa89a1e5e8afe7eb67b8811afe07add
j p <jp@jp.jp>
2022-10-10 03:00:22 +0200

    Some more solutions

--- codewars.js
+++ codewars.js
@@ -1,5 +1,188 @@
 // Codewars solutions
 
+// The Hashtag Generator - 5kyu
+function generateHashtag(str) {
+  if (!str.trim()) return false;
+  const hashedString =
+    "#" +
+    str
+      .split(" ")
+      .filter((s) => s)
+      .map((s) => s[0].toUpperCase() + s.slice(1, s.length))
+      .join("");
+  if (hashedString.length > 140) return false;
+  return hashedString;
+}
+
+// Square Every Digit - 7kyu
+const squareDigits = (num) =>
+  Number(
+    num
+      .toString()
+      .split("")
+      .map((n) => Math.pow(Number(n), 2))
+      .join("")
+  );
+
+// Total amount of points - 8kyu
+function points(games) {
+  let res = 0;
+  games.forEach((g) => {
+    const [x, y] = g.split(":");
+    if (x > y) res += 3;
+    if (x === y) res += 1;
+  });
+  return res;
+}
+
+// The Greatest Warrior - 4kyu
+
+class Warrior {
+  constructor() {
+    this._ranks = [
+      "Pushover",
+      "Novice",
+      "Fighter",
+      "Warrior",
+      "Veteran",
+      "Sage",
+      "Elite",
+      "Conqueror",
+      "Champion",
+      "Master",
+      "Greatest",
+    ];
+    this._battleResponses = [
+      "Invalid level",
+      "Easy fight",
+      "A good fight",
+      "An intense fight",
+      "You've been defeated",
+    ];
+    this._trainingResponses = ["Not strong enough"];
+    this._achievements = [];
+    this._experience = 100;
+    this._level = 1;
+    this._rank = this._ranks[0];
+  }
+
+  achievements() {
+    return this._achievements;
+  }
+
+  battle(enemyLevel) {
+    if (enemyLevel < 1 || enemyLevel > 100) {
+      return this._battleResponses[0];
+    }
+
+    const levelDifference = Math.abs(enemyLevel - this._level);
+    const enemyHigher = enemyLevel - this._level > 0;
+    let response = "";
+
+    if (levelDifference === 0) {
+      this._experience += 10;
+      response = this._battleResponses[2];
+    } else if (!enemyHigher && levelDifference === 1) {
+      this._experience += 5;
+      response = this._battleResponses[2];
+    } else if (!enemyHigher && levelDifference >= 1) {
+      response = this._battleResponses[1];
+    } else if (enemyHigher) {
+      const enemyRank = this._ranks[Math.floor(enemyLevel / 10)];
+      if (this._rank !== enemyRank && levelDifference > 4) {
+        response = this._battleResponses[4];
+      } else {
+        this._experience += 20 * levelDifference * levelDifference;
+        response = this._battleResponses[3];
+      }
+    }
+
+    this._calculateAndSetLevelAndRank();
+    return response;
+  }
+
+  experience() {
+    return this._experience;
+  }
+
+  level() {
+    return this._level;
+  }
+
+  rank() {
+    return this._rank;
+  }
+
+  training([description, experience, minimumLevel]) {
+    if (minimumLevel > this._level) {
+      return this._trainingResponses[0];
+    }
+    this._experience += experience;
+    this._achievements.push(description);
+    this._calculateAndSetLevelAndRank();
+    return description;
+  }
+
+  _calculateAndSetLevelAndRank() {
+    this._level = Math.floor(this._experience / 100);
+    if (this._level > 100) {
+      this._level = 100;
+    }
+    if (this._experience > 10000) {
+      this._experience = 10000;
+    }
+    this._rank = this._ranks[Math.floor(this._level / 10)];
+  }
+}
+
+// How many are smaller than me II? - 3kyu
+// WIP
+
+// function smaller(arr) {
+//   const regexBeginning = "(?<!\\d)";
+//   const regexEnding = "\\d*";
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i === arr.length) continue;
+
+//     let regexMiddle = `-?[0-${arr[i]}]`;
+//     if (arr[i] < 0) regexMiddle = `-[${arr[i]}-9]`;
+
+//     const num = arr[i];
+//     const regex = regexBeginning + regexMiddle + regexEnding;
+
+//     let numRegex = new RegExp(regex, "g");
+
+//     let count = 0;
+//     const remaining = arr.slice(i + 1);
+//     const matches = remaining.join(",").match(numRegex);
+//     console.log(matches);
+//     matches?.forEach((m) => {
+//       if (m < num) count++;
+//     });
+//     arr[i] = count;
+//   }
+//   console.log(arr);
+//   return arr;
+// }
+
+// const res = smaller([1, 1, -1, 0, 0]);
+
+// Working for smaller numbers
+// function smaller(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i === arr.length) return;
+
+//     let count = 0;
+//     const remaining = arr.slice(i + 1);
+//     remaining.forEach((num) => {
+//       if (arr[i] > num) count++;
+//     });
+//     arr[i] = count;
+//   }
+//   return arr;
+// }
+
 // The Hunger Games - Zoo Disaster! - 5kyu
 var whoEatsWho = function (zoo) {
   zoo = zoo.split(",");
@@ -57,6 +240,7 @@ var whoEatsWho = function (zoo) {
 };
 
 // The builder of things - 3kyu
+// WIP
 class Thing {
   constructor(name) {
     this.name = name;
@@ -180,13 +364,6 @@ function deleteNth(arr, n) {
   );
 }
 
-// Shortened
-// const deleteNth = (arr, n) =>
-//   arr.reduce(
-//     (a, b) => ([...a, b].filter((c) => c === b).length > n ? a : [...a, b]),
-//     []
-//   );
-
 // Sort the odd - 6kyu
 function sortArray(array) {
   const isOdd = (val) => val % 2 !== 0;
@@ -218,7 +395,7 @@ Array.prototype.sameStructureAs = function (other) {
   return thisArrayStructure === otherArrayStructure;
 };
 
-// Greed is Good - kyu
+// Greed is Good - 5kyu
 function score(values) {
   const calculateOnesAndFives = (value, factor) =>
     factor * (value === 1 ? 100 : value === 5 ? 50 : 0);
